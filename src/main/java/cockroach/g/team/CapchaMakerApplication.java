5f62ffc5e16d0d3966189668ec5cfb7b327fa70a
airu3 <sugar.tom5310@gmail.com>
2024-10-22 15:12:55 +0900

    Add .gitattributes, application.properties, CapchaMakerApplicationTests.java, .gitignore, CapchaMakerApplication.java, ServletInitializer.java, .mvn/wrapper/maven-wrapper.properties, and pom.xml

---
+++ src/main/java/cockroach/g/team/CapchaMakerApplication.java
@@ -0,0 +1,13 @@
+package cockroach.g.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapchaMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapchaMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cockroach/g/team/CapchaMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package cockroach.g.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapchaMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
