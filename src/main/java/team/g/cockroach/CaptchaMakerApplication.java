2359d03246a5be1f1efb0ea1f523400f87ddd0d6
airu3 <sugar.tom5310@gmail.com>
2024-11-06 09:39:41 +0900

    コードのインデントを修正して可読性を向上させる

--- src/main/java/team/g/cockroach/CaptchaMakerApplication.java
+++ src/main/java/team/g/cockroach/CaptchaMakerApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CaptchaMakerApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(CaptchaMakerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CaptchaMakerApplication.class, args);
+    }
 }
