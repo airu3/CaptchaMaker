6f72b73ed06bd3c93119404ccf53d6091e8a27b4
airu3 <sugar.tom5310@gmail.com>
2024-11-06 13:49:15 +0900

    StageモデルとStageServiceクラスをGameStageとGameStageServiceに置き換えて、コードの整合性を向上させる

--- src/main/java/team/g/cockroach/repository/StageRepository.java
+++ src/main/java/team/g/cockroach/repository/StageRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import team.g.cockroach.model.Stage;
+import java.util.UUID;
+import team.g.cockroach.entity.GameStage;
 
-public interface StageRepository extends JpaRepository<Stage, Long> {
-    Page<Stage> findByCreatorId(Long creatorId, Pageable pageable);
+public interface StageRepository extends JpaRepository<GameStage, UUID> {
+    Page<GameStage> findByCreatorId(Long creatorId, Pageable pageable);
 }
