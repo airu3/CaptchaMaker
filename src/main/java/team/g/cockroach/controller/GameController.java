ae3711cd7e8a31156468bb4a54a6629de5d33a48
airu3 <sugar.tom5310@gmail.com>
2024-11-06 13:49:32 +0900

    GameServiceをGameStageServiceに置き換え、GameStageエンティティを追加してゲームステージ管理機能を実装

--- src/main/java/team/g/cockroach/controller/GameController.java
+++ src/main/java/team/g/cockroach/controller/GameController.java
@@ -3,33 +3,55 @@ package team.g.cockroach.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import team.g.cockroach.service.GameService;
+import team.g.cockroach.service.GameStageService;
 import team.g.cockroach.dto.GameDTO;
 
 @RestController
-@RequestMapping("/api/game")
+@RequestMapping("/game")
 public class GameController {
 
     @Autowired
-    private GameService gameService;
+    private GameStageService gameStageService;
 
+    /**
+     * 指定されたIDのゲームを取得します。
+     * 
+     * @param id ゲームのID
+     * @return ゲームの詳細情報
+     */
     @GetMapping("/{id}")
     public GameDTO getGame(@PathVariable Long id) {
-        return gameService.getGameById(id);
+        return gameStageService.getGameById(id);
     }
 
+    /**
+     * 新しいゲームを作成します。
+     * 
+     * @param gameDTO 作成するゲームの情報
+     */
     @PostMapping
     public void createGame(@RequestBody GameDTO gameDTO) {
-        gameService.createGame(gameDTO);
+        gameStageService.createGame(gameDTO);
     }
 
+    /**
+     * 指定されたIDのゲームを更新します。
+     * 
+     * @param id ゲームのID
+     * @param gameDTO 更新するゲームの情報
+     */
     @PutMapping("/{id}")
     public void updateGame(@PathVariable Long id, @RequestBody GameDTO gameDTO) {
-        gameService.updateGame(id, gameDTO);
+        gameStageService.updateGame(id, gameDTO);
     }
 
+    /**
+     * 指定されたIDのゲームを削除します。
+     * 
+     * @param id ゲームのID
+     */
     @DeleteMapping("/{id}")
     public void deleteGame(@PathVariable Long id) {
-        gameService.deleteGame(id);
+        gameStageService.deleteGame(id);
     }
 }
